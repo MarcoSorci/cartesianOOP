c67ea4dfe5c36d78dcb3dccd6b2e1395d17fccbf
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-02-07 12:26:48 +0100

    asd

--- main.js
+++ main.js
@@ -23,15 +23,17 @@ console.log(pointA.distanceFromOriginP());  //distancefromOriginc
 console.log(pointA.medianPointFromPoint(pointB));  //medianFromB
 console.log(pointA.nearestPoint(pointB, pointC, pointD, pointE, pointF));  //nearestToA
 
-const segment = pointA.distanceFrom(pointB);
+
+const seg1 = new Segment(pointA, pointB)
+// console.log(seg1.length);
+// console.log(seg1.median);
 
 
 
-
-const pointsqA = new Point(1, 1);
-const pointsqB = new Point(1, 3);
-const pointsqC = new Point(6, 1);
-const pointsqD = new Point(6, 3);
+const pointsqA = new Point(1, 1);  //BL
+const pointsqB = new Point(1, 3);  //TL
+const pointsqC = new Point(7, 1);  //BR
+const pointsqD = new Point(7, 3);  //TR
 
 console.log(pointsqA.distanceFrom(pointsqB));
 console.log(pointsqA.distanceFrom(pointsqC));
@@ -42,6 +44,26 @@ console.log(pointsqA.calculateArea(pointsqB, pointsqC));
 console.log(pointsqD.calculateArea(pointsqB, pointsqC));
 console.log(pointsqB.calculateArea(pointsqA, pointsqD));
 
+
+const rect1 = new Rectangle(pointsqA, pointsqB, pointsqC, pointsqD)
+console.log(rect1.perimeter());
+console.log(rect1.area());
+
+const tri1 = new Triangle(pointsqA, pointsqB, pointsqC)
+
+const rect2 = new Rectangle2(pointsqA, 6, 2)
+console.log(rect2.perimeter());
+console.log(rect2.area());
+console.log(rect2.point2());
+console.log(rect2.point3());
+console.log(rect2.point4());
+
+const rect3 = new Rectangle3(pointsqB, pointsqC)
+console.log(rect3.pointTopRight());
+console.log(rect3.pointBottomLeft());
+
+console.log(rect3.perimeter());
+console.log(rect3.area());
 
 
 
--- point.js
+++ point.js
@@ -42,8 +42,8 @@ class Point {
         return points.reduce((p, c) => this.distanceFrom(p) * this.distanceFrom(c))
     }
 
-    medianPointFromSegment(segment){
-        
+    medianPointFromSegment(segment) {
+
     }
 
 
@@ -51,4 +51,132 @@ class Point {
     // calculateArea(p2, p4) {
     //     return this.distanceFrom(p2) * this.distanceFrom(p4)
     // }
+}
+
+class Segment {
+    constructor(point1, point2) {
+        this.point1 = point1
+        this.point2 = point2
+    }
+
+    length(segment) {
+        return
+    }
+
+    median(segment) {
+        return
+    }
+}
+
+class Rectangle {
+    constructor(topLeft, topRight, bottomRight, bottomLeft) {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomRight = bottomRight;
+        this.bottomLeft = bottomLeft;
+    }
+
+    perimeter() {
+        return (this.topLeft.distanceFrom(this.topRight) +
+            this.topRight.distanceFrom(this.bottomRight) +
+            this.bottomRight.distanceFrom(this.bottomLeft) +
+            this.bottomLeft.distanceFrom(this.topLeft)).toFixed()
+    }
+
+    area() {
+        return this.bottomLeft.distanceFrom(this.bottomRight) * this.bottomLeft.distanceFrom(this.topLeft).toFixed()
+    }
+
+}
+
+class Triangle {
+    constructor(point1, point2, point3) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
+    }
+
+    perimeter() {
+        return this.point1.distanceFrom(this.point2) +
+            this.point2.distanceFrom(this.point3) +
+            this.point3.distanceFrom(this.point1);
+    }
+
+    area() {
+        const pd2 = this.perimeter / 2;
+        const pd2MinusFirstSide = pd2 - this.point1.distanceFrom(this.point2);
+        const pd2MinusSecondSide = pd2 - this.point2.distanceFrom(this.point3);
+        const pd2MinusThirdSide = pd2 - this.point3.distanceFrom(this.point1);
+        return Math.sqrt(pd2 * pd2MinusFirstSide * pd2MinusSecondSide * pd2MinusThirdSide)
+    }
+}
+
+class Rectangle2 {
+    constructor(startingpoint, w, h) {
+        this.startingpoint = startingpoint;
+        this.w = w;
+        this.h = h;
+    }
+
+    perimeter() {
+        //return (this.w + this.h) * 2
+        return (this.w * 2) + (this.h * 2)
+    }
+
+    area() {
+        return this.w * this.h
+    }
+
+    point2() {
+        let xOfPoint2 = this.startingpoint.x + this.w
+        let yOfPoint2 = this.startingpoint.y
+        const point2 = new Point(xOfPoint2, yOfPoint2);
+        return point2
+    }
+
+    point3() {
+        let xOfPoint3 = this.point2().x
+        let yOfPoint3 = this.point2().y + this.h
+        const point3 = new Point(xOfPoint3, yOfPoint3);
+        return point3
+    }
+
+    point4() {
+        let xOfPoint4 = this.startingpoint.x
+        let yOfPoint4 = this.startingpoint.y + this.h
+        const point4 = new Point(xOfPoint4, yOfPoint4);
+        return point4
+    }
+}
+
+class Rectangle3 {
+    constructor(topLeft, bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+    }
+
+    pointTopRight() {
+        let xOfPointTR = this.bottomRight.x
+        let yOfPointTR = this.topLeft.y
+        const tr = new Point(xOfPointTR, yOfPointTR);
+        return tr
+    }
+
+    pointBottomLeft() {
+        let xOfPointBL = this.topLeft.x
+        let yOfPointBL = this.bottomRight.y
+        const bl = new Point(xOfPointBL, yOfPointBL);
+        return bl;
+
+    }
+    perimeter() {
+        return (this.topLeft.distanceFrom(this.pointTopRight()) +
+            this.pointTopRight().distanceFrom(this.bottomRight) +
+            this.bottomRight.distanceFrom(this.pointBottomLeft()) +
+            this.pointBottomLeft().distanceFrom(this.topLeft))
+    }
+
+    area() {
+        return this.pointBottomLeft().distanceFrom(this.bottomRight) * this.pointBottomLeft().distanceFrom(this.topLeft)
+    }
 }
